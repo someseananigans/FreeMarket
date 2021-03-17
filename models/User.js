8d56b648017be6d91101ce44c726643879638621
Sean Nguyen <nsean321@gmail.com>
2021-03-16 18:53:42 -0700

    Made a test user

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const sequelize = require('../db')
 
 class User extends Model { }
 
+// Create users Table with columns
 User.init({
   name: DataTypes.STRING,
   email: DataTypes.STRING,
@@ -10,5 +11,17 @@ User.init({
   password: DataTypes.STRING,
   phone: DataTypes.INTEGER
 }, { sequelize, modelName: 'users' })
+
+// Populate table with a row (user)
+User.create({
+  name: 'name',
+  email: 'email@gmail.com',
+  username: 'username',
+  password: 'password',
+  phone: '1234567890'
+})
+  .then(user => { console.log(user) })
+  .catch(err => console.log(err))
+
 
 module.exports = User
