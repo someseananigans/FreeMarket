c1ad8ff614d4afbc3ba908f9bd289f63a08944a8
Sean Nguyen <nsean321@gmail.com>
2021-03-17 11:56:35 -0700

    Search routing

--- routes/listingsRoutes.js
+++ routes/listingsRoutes.js
@@ -8,7 +8,52 @@ router.get('/listings', (req, res) => {
     .catch(err => res.json(listings))
 })
 
-// Get specific listings
+// Get listings by title
+router.get('/listings/:title', (req, res) => {
+  Listing.findAll({ where: { title: req.params.title } })
+    .then(listing => res.json(listing))
+    .catch(listing => res.json(err))
+})
+
+router.get('/listings/search/:title', (req, res) => {
+  Listing.findAll({})
+    .then(listing => {
+      let searchResults = []
+      for (let i = 0; i<listing.length; i++) {
+        if (listing[i].title.includes(req.params.title)) {
+          searchResults.push(listing[i])
+        }
+      }
+      searchResults.sort((a,b) => (a.createdAt-b.createdAt))
+      res.json(searchResults)
+    })
+    .catch(listing => res.json(err))
+})
+
+// Get listings by criteria
+router.get('/listings/:category', (req, res) => {
+  Listing.findAll({ where: { category: req.params.category } })
+    .then(listing => res.json(listing))
+    .catch(listing => res.json(err))
+})
+
+// Get listings by tag
+router.get('/listings/tagsearch/:tag', (req, res) => {
+  Listing.findAll({})
+    .then(listing => {
+      let searchResults = []
+      for (let i = 0; i < listing.length; i++) {
+        if (listing[i].tag.includes(req.params.tag)) {
+          searchResults.push(listing[i])
+        }
+      }
+      searchResults.sort((a, b) => (a.createdAt - b.createdAt))
+      res.json(searchResults)
+    })
+    .catch(listing => res.json(err))
+})
+
+// Get user listings
 router.get('/listings/:user_id', (req, res) => {
   Listing.findAll({ where: { user_id: req.params.user_id } })
     .then(listing => res.json(listing))
