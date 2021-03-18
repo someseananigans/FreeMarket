35420059d28dc691923c33af02a04e3530d0a2ce
Sean Nguyen <nsean321@gmail.com>
2021-03-18 12:00:59 -0700

    Incorporate strategy

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// require('dotenv').config()
+
 const express = require('express')
 const { join } = require('path')
 const sequelize = require('./db')
 const passport = require('passport')
 const { User } = require('./models')
+const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 
 const app = express()
 
@@ -17,6 +20,13 @@ app.use(passport.session())
 passport.use(User.createStrategy())
 passport.serializeUser(User.serializeUser)
 passport.deserializeUser(User.deserializeUser)
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET
+}, ({ id }, cb) => User.findOne({ id })
+  .then(user => cb(null, user))
+  .catch(err => cb(err))))
 
 app.use(require('./routes'))
 
