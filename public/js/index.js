aab27fc4edcb66dfa6cae7f675a66ffb878cac39
Joseph Nielsen <josephmnielsen@gmail.com>
2021-03-20 19:39:43 -0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,95 @@
 
+const getListings = () => {
+  axios.get('/api/listings')
+
+  .then(({ data: listings }) => {
+    document.getElementById('listings').innerHTML = ''
+    listings.forEach(listing => {
+      const listingElem = document.createElement('div')
+      listingElem.className = 'col s12 m6 l4'
+      listingElem.innerHTML = `
+        <div class="row">
+         <div class="col s12 m7">
+           <div class="card">
+             <div class="card-image">
+                <img src="../images/${listing.image}">
+                
+              </div>
+              <span class="card-title">${listing.title}</span>
+            </div>
+          </div>
+        </div>
+      `
+      document.getElementById('listings').append(listingElem)
+
+      })
+  })
+  .catch(err => console.error(err))
+}
+
+getListings()
+
+document.getElementById('automotive').addEventListener('click', event => {
+
+  axios.get(`/api/listings/automotive`)
+    
+  .then(({ data: listings }) => {
+    document.getElementById('listings').innerHTML = ''
+    listings.forEach(listing => {
+      const autoElem = document.createElement('div')
+      autoElem.className = 'col s12 m6 l4'
+      autoElem.innerHTML = `
+        <div class="row">
+         <div class="col s12 m7">
+           <div class="card">
+             <div class="card-image">
+                <img src="../images/${listing.image}">
+                
+              </div>
+              <span class="card-title">${listing.title}</span>
+            </div>
+          </div>
+        </div>
+      `
+      document.getElementById('listings').append(autoElem)
+
+    })
+  })
+  .catch(err => console.error(err))
+})
+
+document.getElementById('search').addEventListener('input', event => {
+
+  axios.get(
+    `/api/listings/search/`
+    )
+
+    .then(({ data: listings }) => {
+      document.getElementById('listings').innerHTML = ''
+      listings.forEach(listing => {
+        const autoElem = document.createElement('div')
+        autoElem.className = 'col s12 m6 l4'
+        autoElem.innerHTML = `
+        <div class="row">
+         <div class="col s12 m7">
+           <div class="card">
+             <div class="card-image">
+                <img src="../images/${listing.image}">
+                
+              </div>
+              <span class="card-title">${listing.title}</span>
+            </div>
+          </div>
+        </div>
+      `
+        document.getElementById('listings').append(autoElem)
+
+      })
+    })
+    .catch(err => console.error(err))
+})
+
+
 
 // get all listings
 // let getAllListings = () => {
