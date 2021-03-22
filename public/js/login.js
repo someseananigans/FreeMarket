d3253d8b420063325846eb4b4967039636fa62f4
Sean Nguyen <nsean321@gmail.com>
2021-03-21 19:58:01 -0700

    Added script for login.html

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,51 @@
+const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
-
-document.getElementById('profileSubmit').addEventListener('click', event => {
-  event.preventDefault()
-
-    axios.post('/api/user', {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      username: document.getElementById('username').value,
-      password: document.getElementById('password').value,
-      phone: document.getElementById('phone').value
+document.getElementById('signUp').addEventListener('click', event => {
+  axios.get('/api/usersnames')
+    .then(({ data: usernames }) => {
+      let newUser = {
+        name: document.getElementById('nameS').value,
+        phone: document.getElementById('phoneS').value,
+        password: document.getElementById('passwordS').value,
+        email: document.getElementById('emailS').value
+      }
+      if (!validateEmail(document.getElementById('emailS').value)) {
+        document.getElementById('invalidEmail').innerHTML = 'Invalid email'
+      }
+      if (!usernames.includes(document.getElementById('usernameS').value)) {
+        newUser.username = document.getElementById('usernameS').value
+      } else {
+        document.getElementById('invalidUser').innerHTML = 'Username already exists'
+        document.getElementById('usernameS').classList.add('invalid')
+      }
+      if (newUser.email && newUser.username) {
+        axios.post('/api/user/register', newUser)
+          .then(() => window.location = '/login')
+          .catch(err => {
+            console.log(err)
+          })
+      }
     })
-    .then(() => {
-      document.getElementById('name').value = ''
-      document.getElementById('email').value = ''
-      document.getElementById('username').value = ''
-      document.getElementById('password').value = ''
-      document.getElementById('phone').value = ''
-    })
-  .catch(err => console.error(err))
 })
+
+document.getElementById('login').addEventListener('click', event => {
+  axios.post('/api/user/login', {
+    username: document.getElementById('usernameL').value,
+    password: document.getElementById('passwordL').value
+  })
+    .then(({ data: token }) => {
+      if (token) {
+        localStorage.setItem('token', token)
+        window.location = '/'
+      } else {
+        document.getElementById('invalid').innerHTML = "Username and/or Password were incorrect. Please try again."
+        document.getElementById('usernameL').classList.add('invalid')
+        document.getElementById('passwordL').classList.add('invalid')
+      }
+    })
+    .catch(err => console.log(err))
+})
+
