cb37a8469dfc47b319bcc1945134e3498b84a973
Sean Nguyen <nsean321@gmail.com>
2021-03-23 16:49:04 -0700

    Update with current html routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,12 +5,16 @@ router.get('/profile', (req, res) => {
   res.sendFile(join(__dirname, '..', 'public', 'profile.html'))
 })
 
-router.get('/listing', (req, res) => {
-  res.sendFile(join(__dirname, '..', 'public', 'listing.html'))
-})
-
 router.get('/login', (req, res) => {
   res.sendFile(join(__dirname, '..', 'public', 'login.html'))
+})
+
+router.get('/createPost', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'createPost.html'))
+})
+
+router.get('/editProfile', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'editProfile.html'))
 })
 
 router.get('/*', (req, res) => {
