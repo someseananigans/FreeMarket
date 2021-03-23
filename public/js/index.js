893c304394a309bded640d2d2cbbd26a1b148d0b
Sean Nguyen <nsean321@gmail.com>
2021-03-23 16:47:35 -0700

    firebase adjustment

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,7 @@ const getListings = () => {
          <div class="col s12 m7">
            <div class="card hoverable" id="cardItem">
              <div class="card-image">
-                <img src="../images/${listing.image}" height="175px" width="auto">
+                <img src="${listing.image}" height="175px" width="auto">
                 
               </div>
               <span class="card-title">${listing.title}</span>
