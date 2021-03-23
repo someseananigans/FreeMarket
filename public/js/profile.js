c3676ad2b5963fc79b5c53d42f02542d2e6252e5
Kyle Ohlwiler <kohlwiler@yahoo.com>
2021-03-23 13:56:33 -0700

    added comment lines and cleaned up code

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,11 +10,11 @@ const getMyListings = () => {
       "Authorization": `Bearer ${token}`
     }
     })
+    // grabs all listings from user and posts in a card
   .then(({ data: { listings }}) => {
     console.log(listings)
     document.getElementById('items').innerHTML = ''
       listings.forEach(listing => {
-        console.log(listing)
         let listingElem = document.createElement('div')
         listingElem.className = 'col s9 l6'
         listingElem.innerHTML = `
@@ -29,79 +29,16 @@ const getMyListings = () => {
           </div>
         `
       document.getElementById('items').append(listingElem)
-
-
       })
   })
   .catch(err => console.log(err))
 }
 
-// document.getElementById('saveUpdate').addEventListener('click', event => {
-//   const id = event.target.dataset.id
-//   axios.put(`/api/listings/${id}`, {
-//     title: document.getElementById('uTitle').value,
-//     description: document.getElementById('uDescription').value,
-//     img: document.getElementById('uImg').value
-//   })
-//     .then(() => {
-//       getMyListings()
-//     })
-//     .catch(err => console.log(err))
-// })
 // sign Out
 document.getElementById('signOut').addEventListener('click', event => {
   localStorage.removeItem('token')
   window.location = '/login.html'
 })
-
-// creates form to edit profile name and username
-document.getElementById('editProfile').addEventListener('click', event => {
-  document.getElementById('items').innerHTML = ''
-  let getItemInfo = document.createElement('div')
-  getItemInfo.className =
-    getItemInfo.innerHTML = `
-  
- <form class="col s12" id="getinfo">
-    <div class="row">
-      <div class="input-field col s12">
-        <input id="name" type="text" class="validate">
-        <label for="title">Name</label>
-      </div>
-    </div>
-    <div class="row">
-      <form class="col s12">
-        <div class="row">
-          <div class="input-field col s12">
-            <textarea id="email" class="materialize-textarea"></textarea>
-            <label for="textarea1">Email</label>
-          </div>
-        </div>
-        </form>
-      </div> 
-    </div>
-  </form>
- <button class="btn" type="submit" name="action" id="updateProfile">Save
- <i class="material-icons right">send</i>
- </button>
- 
- `
-  document.getElementById('items').append(getItemInfo)
-
-  document.getElementById('updateProfile').addEventListener('click', event => {
-
-    axios.put('/api/user', {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      username: document.getElementById('username').value,
-      phone: document.getElementById('phone').value
-    })
-      .then(() => {
-        window.location = '/profile.html'
-      })
-      .catch(err => console.log(err))
-  })
-})
-
 
 // global event listener
 document.addEventListener('click', event => {
@@ -117,7 +54,7 @@ document.addEventListener('click', event => {
       })
       .catch(err => console.log(err))
   }
-  // Save update modal
+  // Save item update modal
   if (event.target.id === 'saveUpdate') {
     let token = localStorage.getItem('token')
     const id = event.target.dataset.id
