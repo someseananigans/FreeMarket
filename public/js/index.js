a6288d8340b1b2a0f7385638fc839ca0fdfc9940
Sean Nguyen <nsean321@gmail.com>
2021-03-25 11:22:54 -0700

    Open Modals

--- public/js/index.js
+++ public/js/index.js
@@ -3,30 +3,25 @@
 const getListings = () => {
   axios.get('/api/listings')
 
-  .then(({ data : listings }) => {
-    console.log(listings)
-    document.getElementById('listings').innerHTML = ''
-    listings.forEach(listing => {
-      let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
-      const listingElem = document.createElement('div')
-      listingElem.className = 'col s12 m6 l4'
-      listingElem.innerHTML = `
+    .then(({ data: listings }) => {
+      console.log(listings)
+      document.getElementById('listings').innerHTML = ''
+      listings.forEach(listing => {
+        let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
+        const listingElem = document.createElement('div')
+        listingElem.className = 'col s12 m6 l4'
+        listingElem.innerHTML = `
 
-           <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
-             <div class="card-image">
-                <img src="${listing.image}" height="175px" width="auto">
-                
-              </div>
-              <h3 class="card-title center cardTitle">${listingTitle}</h3>
-            </div>
+          <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
+            <img class="card-iamge" src="${listing.image}" height="175px" width="auto">
+            <h3 class="card-title center cardTitle">${listingTitle}</h3>
           </div>
-        </div>
       `
-      document.getElementById('listings').append(listingElem)
+        document.getElementById('listings').append(listingElem)
 
+      })
     })
-  })
-  .catch(err => console.error(err))
+    .catch(err => console.error(err))
 }
 
 getListings()
@@ -34,14 +29,14 @@ getListings()
 document.getElementById('automotive').addEventListener('click', event => {
 
   axios.get(`/api/listings/category/automotive`)
-    
-  .then(({ data: listings }) => {
-    document.getElementById('listings').innerHTML = ''
-    listings.forEach(listing => {
-      let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
-      const autoElem = document.createElement('div')
-      autoElem.className = 'col s12 m6 l4'
-      autoElem.innerHTML = `
+
+    .then(({ data: listings }) => {
+      document.getElementById('listings').innerHTML = ''
+      listings.forEach(listing => {
+        let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
+        const autoElem = document.createElement('div')
+        autoElem.className = 'col s12 m6 l4'
+        autoElem.innerHTML = `
 
            <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
              <div class="card-image">
@@ -53,11 +48,11 @@ document.getElementById('automotive').addEventListener('click', event => {
           </div>
         </div>
       `
-      document.getElementById('listings').append(autoElem)
+        document.getElementById('listings').append(autoElem)
 
+      })
     })
-  })
-  .catch(err => console.error(err))
+    .catch(err => console.error(err))
 })
 document.getElementById('household').addEventListener('click', event => {
 
@@ -257,17 +252,17 @@ document.getElementById('random').addEventListener('click', event => {
 })
 
 document.getElementById('search1').addEventListener('input', event => {
-  
+
   if (document.getElementById('search1').value) {
     axios.get(`/api/listings/search/${document.getElementById('search1').value}`)
 
-    .then(({ data: listings }) => {
-      document.getElementById('listings').innerHTML = ''
-      listings.forEach(listing => {
-        const autoElem = document.createElement('div')
-        let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
-        autoElem.className = 'col s12 m6 l4'
-        autoElem.innerHTML = `
+      .then(({ data: listings }) => {
+        document.getElementById('listings').innerHTML = ''
+        listings.forEach(listing => {
+          const autoElem = document.createElement('div')
+          let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
+          autoElem.className = 'col s12 m6 l4'
+          autoElem.innerHTML = `
 
            <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
              <div class="card-image">
@@ -279,12 +274,12 @@ document.getElementById('search1').addEventListener('input', event => {
           </div>
         </div>
       `
-        document.getElementById('listings').append(autoElem)
-        
+          document.getElementById('listings').append(autoElem)
+
+        })
+
       })
-      
-    })
-    .catch(err => console.error(err))
+      .catch(err => console.error(err))
 
   } else {
     getListings()
@@ -292,34 +287,33 @@ document.getElementById('search1').addEventListener('input', event => {
 })
 
 document.getElementById('search2').addEventListener('input', event => {
-  
+
   if (document.getElementById('search2').value) {
     axios.get(`/api/listings/search/${document.getElementById('search2').value}`)
 
-    .then(({ data: listings }) => {
-      document.getElementById('listings').innerHTML = ''
-      listings.forEach(listing => {
-        const autoElem = document.createElement('div')
-        let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
-        autoElem.className = 'col s12 m6 l4'
-        autoElem.innerHTML = `
+      .then(({ data: listings }) => {
+        document.getElementById('listings').innerHTML = ''
+        listings.forEach(listing => {
+          const autoElem = document.createElement('div')
+          let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
+          autoElem.className = 'col s12 m6 l4'
+          autoElem.innerHTML = `
 
            <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
              <div class="card-image">
                 <img src="${listing.image}" height="175px" width="auto">
-                
               </div>
               <h3 class="card-title center cardTitle">${listingTitle}</h3>
             </div>
           </div>
         </div>
       `
-        document.getElementById('listings').append(autoElem)
-        
+          document.getElementById('listings').append(autoElem)
+
+        })
+
       })
-      
-    })
-    .catch(err => console.error(err))
+      .catch(err => console.error(err))
 
   } else {
     getListings()
@@ -330,7 +324,34 @@ document.addEventListener('click', event => {
 
   if (localStorage.getItem('token')) {
 
-    if (event.target.classList.contains('listings')) {
+    if (event.target.parentNode.classList.contains('listings')) {
+      const id = event.target.parentNode.dataset.id
+      axios.get(`/api/listings/id/${id}`)
+
+        .then(({ data: listing }) => {
+          console.log(listing)
+          let listingTitle = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
+          document.getElementById('listFull').innerHTML =
+            `
+      <div class="row center">
+        <p id="listImage"><img src="${listing.image}" height="175px" width="auto"></p>
+      </div>
+      <div class="row">
+        <h4 id="listTitle">${listingTitle}</h4>
+      </div>
+      <div class="row">
+        <p id="listDesc">${listing.description}</p>
+      </div>
+      <a href="mailto:${listing.User.email}">
+      <button class="btn modal-close waves-effect waves-green" type="email" name="action">
+      <i class="material-icons right">email</i>
+      </button>
+      </a>
+      `
+        })
+        .catch(err => console.log(err))
+    }
+    else if (event.target.classList.contains('listings')) {
       const id = event.target.dataset.id
       axios.get(`/api/listings/id/${id}`)
 
@@ -354,12 +375,12 @@ document.addEventListener('click', event => {
       </button>
       </a>
       `
-      })
-      .catch(err => console.log(err))
-    } else {}
+        })
+        .catch(err => console.log(err))
+    }
 
   } else {
     window.location = '/login.html'
   }
-  
+
 })
